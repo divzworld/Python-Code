76795d2285d65aae7d0ea4d05c88e9182ebd70f3
Divya Yadav <ydivya148@gmail.com>
2024-12-06 14:57:43 +0530

    Swapping numbers

---
+++ swap1.py
@@ -0,0 +1,13 @@
+# Que. Write a program to swap two numbers using a temporary variable 
+
+# Before swapping
+num1 = 10
+num2 = 20
+temp = None
+print(f"Before swapping Those two numbers are num1 :{num1} , num2 :{num2}")
+
+# Code for swap the numbers
+temp = num1
+num1 = num2
+num2 = temp
+print(f"After swapping Those two numbers are num1 :{num1} , num2 :{num2}")
